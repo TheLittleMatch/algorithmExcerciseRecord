abef3dc4fc75f451e83380a6c917dadb7d278cad
zhou <zhoushunqiang7@163.com>
2019-07-31 21:07:45 +0800

    first chapter

---
+++ aha/AbarrelSort.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// O(n+m)
+int main() {
+  int a[1001], n, i, t;
+  for (i = 1; i <= 1000; i++) {
+    a[i] = 0;
+    /* code */
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+      scanf("%d", &t);
+      a[t] = 1;
+    }
+    for (i = 1; i <= n; i++) {
+      if (a[i] == 1) {
+        printf("%d ", i);
+      }
+    }
+
+    getchar();
+    getchar();
+    return 0;
+  }
--- aha/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() {
-
-  printf("aaa");
-  return 0;
-}
